8a3188ab25e899941f5c5612a3ac126c239166b0
imran ansari <stasim101@gmail.com>
2020-12-08 08:06:42 +0530

    Changing the API

--- bandit/src/main/java/com/personal/bandit/service/MessageExtractorService.java
+++ bandit/src/main/java/com/personal/bandit/service/MessageExtractorService.java
@@ -1,65 +1,59 @@
 package com.personal.bandit.service;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.apache.commons.lang.StringUtils;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import com.personal.bandit.model.Bandit;
+import com.personal.bandit.repository.BanditRepository;
 
 public class MessageExtractorService {
 
-	public String messageExtractor(String message) {
+	static String tenant;
 
-		if (message.contains("Organization")) {
-			Pattern pattern = Pattern.compile(
-					"(\\p{Alnum}[\\p{Alnum}\\s/]+?)\\s?(:|\\.+)\\s?(\\p{Alnum}[\\p{Alnum}\\s/]+?)(?=($|\\()|(\\s\\())",
-					Pattern.MULTILINE);
-			Matcher matcher = pattern.matcher(message);
-			while (matcher.find()) {
-				System.out.println(matcher.group(1) + "-->" + matcher.group(3));
-			}
+	@Autowired
+	private BanditRepository banditRepository;
+
+	public void messageExtractor(String message) {
+
+		if (message.contains("Organization:"))
+			tenant = StringUtils.substringAfter(message, "Organization:").trim();
+
+		else {
+
+			Bandit bandit = new Bandit();
+
+			String key = "More Info:";
+			bandit.setMoreInfo(StringUtils.substringAfter(message, key).trim());
+			message = StringUtils.substringBefore(message, key);
+
+			key = "Location:";
+			bandit.setLocation(StringUtils.substringAfter(message, key).trim());
+			message = StringUtils.substringBefore(message, key);
+
+			key = "Confidence:";
+			bandit.setConfidence(StringUtils.substringAfter(message, key).trim());
+			message = StringUtils.substringBefore(message, key);
+
+			key = "Severity:";
+			bandit.setSeverity(StringUtils.substringAfter(message, key).trim());
+			message = StringUtils.substringBefore(message, key);
+
+			key = "Issue:";
+			bandit.setIssue(StringUtils.substringAfter(message, key).trim());
+
+			bandit.setTenantId(tenant);
+
+			banditRepository.save(bandit);
 		}
 
-		return null;
 	}
 
 	public static void main(String[] args) {
 
-		MessageExtractorService ins = new MessageExtractorService();
-		String s = "Issue: [B404:blacklist] Consider possible security implications associated with subprocess module.  "
+		String message = "Issue: [B404:blacklist] Consider possible security implications associated with subprocess module.  "
 				+ " Severity: Low" + " Confidence: High " + " Location: fportaintier\\vulpy\\bad\\brute.py:3"
 				+ " More Info: https://bandit.readthedocs.io/en/latest/blacklists/blacklist_imports.html"
 				+ "#b404-import-subprocess2  " + "3       import subprocess" + "4       import sys";
 
-		Bandit bandit = new Bandit();
-
-		String key = "More Info:";
-		bandit.setMoreInfo(StringUtils.substringAfter(s, key).trim());
-		s = StringUtils.substringBefore(s, key);
-
-		key = "Location:";
-		bandit.setLocation(StringUtils.substringAfter(s, key).trim());
-		s = StringUtils.substringBefore(s, key);
-
-		key = "Confidence:";
-		bandit.setConfidence(StringUtils.substringAfter(s, key).trim());
-		s = StringUtils.substringBefore(s, key);
-
-		key = "Severity:";
-		bandit.setSeverity(StringUtils.substringAfter(s, key).trim());
-		s = StringUtils.substringBefore(s, key);
-
-		key = "Issue:";
-		bandit.setIssue(StringUtils.substringAfter(s, key).trim());
-
-		// ins.messageExtractor(s);
-
-		String h = "2020-12-07 06:58:40.320  INFO 3428 --- [ntainer#0-0-C-1] c.p.bandit.service.KafkaConsumerService  : #### -> Consumed message -> Organization: google";
-		h = StringUtils.substringAfter(h, "Organization:").trim();
-
-		System.out.println(h);
+		
 	}
 }
