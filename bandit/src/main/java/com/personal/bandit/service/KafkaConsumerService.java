c84f0ba7c9dd9562804d6cf1ce7287e7c26d2e71
imran ansari <stasim101@gmail.com>
2020-12-10 15:55:51 +0530

    Removing line 24 from KafkaConsumerService.java

--- bandit/src/main/java/com/personal/bandit/service/KafkaConsumerService.java
+++ bandit/src/main/java/com/personal/bandit/service/KafkaConsumerService.java
@@ -1,15 +1,11 @@
 package com.personal.bandit.service;
 
 import java.io.IOException;
-
-import javax.validation.ReportAsSingleViolation;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
 import com.personal.bandit.repository.BanditRepository;
 
 @Service
@@ -25,7 +21,6 @@ public class KafkaConsumerService {
 
 	@KafkaListener(topics = "bandit", groupId = "group_id")
 	public String consume(String message) throws IOException {
-		logger.info(String.format("Consumed message -> %s", message));
 		messageExtractorService.saveExtract(message);
 		return message;
 	}
